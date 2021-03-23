3a5ccc862179c18a2c1bbd81afbe7b50deaaf6b3
Damien K <73603228+Dami-Adebambo@users.noreply.github.com>
2021-03-23 12:57:53 +0000

    Create StartUp.java

---
+++ src2/StartUp.java
@@ -0,0 +1,22 @@
+public class StartUp{
+  Private SmapleProvider cs;
+  Private SampleProvider ts;
+  public StartUp(EV3touch ts, EV3ColorSensor cs){
+    this.cs = cs.getRedMode();
+    this.ts = ts.getTouchMode();
+  }
+  
+  public void start(){
+    Calibrate cal_cs = new AbstractCalibrationFilter(this.cs) {};
+    cal_cs.startCalibration();
+		Thread.sleep(2000);
+		cal_cs.stopCalibration();
+    
+		Calibrate cal_ts = new AbstractCalibrationFilter(this.ts) {};
+    cal_ts.startCalibration();
+		Thread.sleep(2000);
+		cal_ts.stopCalibration();
+    
+  }
+  
+}
