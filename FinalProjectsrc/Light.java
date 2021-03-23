6572e34a48b36120345e9d53574a6333e408fee4
Damien K <73603228+Dami-Adebambo@users.noreply.github.com>
2021-03-23 16:06:15 +0000

    Update Light.java

--- FinalProjectsrc/Light.java
+++ FinalProjectsrc/Light.java
@@ -8,7 +8,6 @@ import lejos.robotics.navigation.MovePilot;
 public class Light implements Behavior {
 	private float maxSpeed = 720;
 	private MovePilot pilot;
-	private EV3ColorSensor cs;
 	private SampleProvider sp;
 	float[] colour;
   	private SampleProvider sp2 = cs2.getRedMode();
@@ -17,9 +16,8 @@ public class Light implements Behavior {
 	//float[] colour2 = new float[1];
 	//private EV3ColorSensor cs2;
 	
-	Light(MovePilot p,float[] min, EV3ColorSensor cs, File file)	{
+	Light(MovePilot p, float[] min, EV3ColorSensor cs, File file)	{
 		this.pilot = p;
-		this.cs = cs;
     		this.sp = cs.getRedMode();
     		this.color = min;
     		this.file = file; 
@@ -32,9 +30,9 @@ public class Light implements Behavior {
   
   
   	public boolean takeControl() {
-			//is.sp2.fetchSample(colour2, 0);
-			return(this.color[0] > this.sp.fetchSample(color , 0));
-		}
+		//is.sp2.fetchSample(colour2, 0);
+		return(this.color[0] > this.sp.fetchSample(color , 1));
+	}
   
   	public void playTune() {
     	int time = Sound.playSample(this.file, 20);
