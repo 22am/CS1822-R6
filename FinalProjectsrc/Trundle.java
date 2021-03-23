303149d8d8ffef45e41fd52bfd042e7dcbecab49
Damien K <73603228+Dami-Adebambo@users.noreply.github.com>
2021-03-23 14:38:57 +0000

    Create Trundle.java

---
+++ FinalProjectsrc/Trundle.java
@@ -0,0 +1,17 @@
+import lejos.robotics.navigation.MovePilot;
+import lejos.robotics.subsumption.Behavior;
+
+public class Trundle implements Behavior {
+	private MovePilot pilot;
+	Trundle(MovePilot p) {
+		this.pilot  = p;
+	}
+	public boolean takeControl() {
+		return true;
+	}
+	public void action() {
+		pilot.forward();
+	}
+	public void suppress() {}
+
+}
