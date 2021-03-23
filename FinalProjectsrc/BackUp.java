f9a56b83036c452b5947e5937ba031cd3ba59b3d
Damien K <73603228+Dami-Adebambo@users.noreply.github.com>
2021-03-23 14:10:13 +0000

    Create BackUp.java

---
+++ FinalProjectsrc/BackUp.java
@@ -0,0 +1,42 @@
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+import lejos.robotics.navigation.MovePilot;
+import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
+import java.util.Random;
+
+public class BackUp implements Behavior{
+	private EV3TouchSensor sensor;
+	private MovePilot pilot;
+	private SampleProvider sp;
+	private float[] sample = new float[1];
+	
+	BackUp(MovePilot pilot,EV3TouchSensor sensor){
+		this.sensor = sensor;
+		this.sp = sensor.getTouchMode();
+		this.pilot = pilot;
+	}
+	
+	public boolean takeControl() {
+		sp.fetchSample(sample,0);
+		return sample[0] < 0.5f;
+	}
+
+
+	public void action() {
+		Random rand = new Random();
+		this.pilot.backward();
+		Delay.msDelay(1500); // 1.5s
+		
+		if (rand.nextBoolean()) {
+			this.pilot.rotate(-510);
+		}
+		else {
+			this.pilot.rotate(510);
+		}
+	}
+
+
+	public void suppress() {}
+
+}
